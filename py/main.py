dee7beb53cfc80781e6bbb8022f04ac48a4bc65f
lorenzo-canton <99317640+lorenzo-canton@users.noreply.github.com>
2022-03-24 15:51:12 +0100

    Modularità +
    - attributo data nell'oggetto trader da passare al thread
    - corretto errore print del dataframe

--- py/main.py
+++ py/main.py
@@ -19,47 +19,51 @@ TOKEN = 'f8b1b32250896804e7398ce31a2106dee70e4eef'
 
 
 timeframe = {
-    'm1' : 1,
-    'm5' : 5,
-    'm15' : 15,
-    'h1' : 60,
-    'h2' : 2 * 60,
-    'h3' : 3 * 60,
-    'h4' : 4 * 60,
-    'h6' : 6 * 60,
-    'h8' : 8 * 60,
-    'D1' : 24 * 60,
-    'W1' : 7 * 24 * 60,
-    'M1' : 30 * 24 * 60
+    'm1': 1,
+    'm5': 5,
+    'm15': 15,
+    'h1': 60,
+    'h2': 2 * 60,
+    'h3': 3 * 60,
+    'h4': 4 * 60,
+    'h6': 6 * 60,
+    'h8': 8 * 60,
+    'D1': 24 * 60,
+    'W1': 7 * 24 * 60,
+    'M1': 30 * 24 * 60
 }
 
 
-def get_data(con):
-    return con.get_candles('GBP/JPY', period='D1', number=20)
+def get_data(connection, sym, per, num):
+    return connection.get_candles(sym, period=per, number=num)
 
 
-def thread_function(name, con, tf):
+def thread_function(name, connection, data):
     con.logger.disabled = True
     while True:
         logging.info("Thread %s", name)
-        logging.info('\n' + get_data(con))
-        time.sleep(timeframe[tf] * 60)
+        print(get_data(connection))
+        time.sleep(timeframe[data['tf']] * 60)
 
 
 if __name__ == "__main__":
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+    log_format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=log_format, level=logging.INFO, datefmt="%H:%M:%S")
 
     con = fxcmpy.fxcmpy(access_token=TOKEN, log_level='error', log_file=None)
-    con.logger.disabled = True
+
     traders = [
         {
-            'target' : thread_function,
-            'name' : 'api',
-            'tf' : 'm1'
+            'target': thread_function,
+            'name': 'api',
+            'data': {
+                'sym': 'GBP/JPY',
+                'tf': 'm1',
+                'number': 20
+            }
         }
     ]
 
     for i in traders:
-        x = threading.Thread(target=i['target'], args=(i['name'], con, i['tf'],))
+        x = threading.Thread(target=i['target'], args=(i['name'], con, i['data'],))
         x.start()
