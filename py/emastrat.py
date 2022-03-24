3b5395ce6e646c894d2700c3d695102d84ed5a58
lorenzo-canton <99317640+lorenzo-canton@users.noreply.github.com>
2022-03-24 20:09:08 +0100

    Calcolo del trend

--- py/emastrat.py
+++ py/emastrat.py
@@ -4,32 +4,41 @@ import time
 
 
 from apihandler import get_data
-from ENV import timeframe, ema_fast_number
+from ENV import timeframe, ema_fast_number, ema_slow_number
 
 
 class EmaTrader(threading.Thread):
-
-  def ema(self, close, last):
-    return (close - last) * (self.alpha + last)
   
   def __init__(self, symbol, timeframe):
     threading.Thread.__init__(self)
-    log_format = "%(asctime)s: %(message)s"
     self.symbol = symbol
     self.timeframe = timeframe
-    self.alpha = 2/(5+1)
+    
     
   def run(self):
     while True:
       logging.info("EMA TRADER")
-      df = get_data(self.symbol, self.timeframe, ema_fast_number)
+      df = get_data(self.symbol, self.timeframe, ema_slow_number)
 
-      #ema_fast = df['askclose'].mean()
-      #df['mva_fast'] = df['askclose'].ewm(span=ema_fast_number)
-      ema10 = df['bidclose'].ewm(span=10).mean()
-      print(ema10)
-
-
+      # calcolo ema veloce e lenta
+      ema_fast = df['bidclose'].ewm(span=ema_fast_number).mean()
+      ema_slow = df['bidclose'].ewm(span=ema_slow_number).mean()
+      
+      # calcolo trend attuale
+      if ema_fast[-1] > ema_slow[-1]:
+        actual_trend = 'up'
+      else:
+        actual_trend = 'down'
+      
+      # calcolo trend passato
+      if ema_fast[-2] > ema_slow[-2]:
+        last_trend = 'up'
+      else:
+        last_trend = 'down'
+      
+      # controllo se il trend cambia
+      if last_trend != actual_trend:
+        print(actual_trend)
 
       time.sleep(timeframe[self.timeframe] * 60)
     
